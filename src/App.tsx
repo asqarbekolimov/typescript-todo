7e2d8172e96569eda88b955c9ab83232bc993692
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-02-28 17:03:23 +0500

    Finish todo app

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent, useState } from "react";
+import { IData } from ".";
+import { data } from "./constatnts";
+import styles from "./home.module.css";
+
+const App = (): JSX.Element => {
+  const [title, setTitle] = useState<string>();
+  const [arr, setArr] = useState<IData[]>(data);
+
+  const handlerInput = (e: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.target.value);
+  };
+
+  const handlerSubmit = (): void => {
+    if (!title?.length) return;
+    const newData = {
+      title: title,
+      id: new Date().getTime(),
+      description: "description",
+    };
+    console.log(newData);
+    setArr([...arr, newData]);
+    setTitle('');
+  };
+
+  const deletItem = (id: number): void => {
+    const newData = arr.filter((c) => c.id != id);
+    setArr(newData);
+  };
+
+  return (
+    <div className={styles.todo}>
+      <h1 className={styles.title}>App Todo</h1>
+      <input
+        type="text"
+        placeholder="Enter todo"
+        className={styles.input}
+        onChange={handlerInput}
+      />
+      <button onClick={handlerSubmit} className={styles.button}>
+        Add Todo
+      </button>
+
+      <div className={styles.card}>
+        {arr.map((c) => (
+          <div key={c.id} className={styles.itemCard}>
+            <p>{c.title}</p>
+            <div className={styles.delBtn}>
+              <button onClick={() => deletItem(c.id)}>Del</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constatnts/index.ts
@@ -0,0 +1,6 @@
+import { IData } from "./../index";
+export const data: IData[] = [
+  { title: "Omar", id: 1, description: "Description" },
+  { title: "Osman", id: 2, description: "Description" },
+  { title: "Abdulloh", id: 3, description: "Description" },
+];
